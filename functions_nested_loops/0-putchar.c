c0b1db7f73ddca24c32b74b55070f1087b6e26ba
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-02-18 09:52:02 +0100

    Task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,15 +2,6 @@
 
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
