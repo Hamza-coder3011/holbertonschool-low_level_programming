4a3f94e0fc9e22867440e49c1d3fd12045c0b38a
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-02-24 10:23:21 +0100

    Task0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* @c: character to check
+*
+* main - Fonctions that cheks for uppercase character
+*
+* Return: gives 1 or 0
+*/
+int main(void)
+
+int _isupper(int c);
+{
+	if (c >= 'a' && c <='z')
+	{
+		return (1);
+	}
+	return (0);
+}
