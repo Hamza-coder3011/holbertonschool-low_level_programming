72109fbc3020cd9132257185a544ffc9440ff3f2
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-02-28 09:26:01 +0100

    Task3

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "main.h"
+
 
 /**
  * main - check the code
@@ -7,9 +9,11 @@
  */
 int main(void)
 {
-    char *str;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strcmp - Compares two strings.
+* @s1: The first string.
+* @s2: The second string.
+*
+*
+* Return: 0 if strings are equal, a positive value if s1 > s2, and a negative value if s1 < s2.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+	{
+		return *s1 - *s2;
+	}
+	
+	
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
