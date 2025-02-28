0c061d5309cf64407d4e5dba10cf16ef9d1f36fe
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-02-28 09:30:39 +0100

    Task3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,8 @@
 * _strcmp - Compares two strings.
 * @s1: The first string.
 * @s2: The second string.
-*
-*
-* Return: 0 if strings are equal, a positive value if s1 > s2, and a negative value if s1 < s2.
+* Return: 0 if strings are equal, a positive value if s1 > s2,
+* and a negative value if s1 < s2.
 */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,10 +13,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 != *s2)
 	{
-		return *s1 - *s2;
+		return (*s1 - *s2);
 	}
-	
-	
 		s1++;
 		s2++;
 	}
