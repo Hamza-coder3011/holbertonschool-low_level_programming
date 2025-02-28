ce5119864215bcbfbd67c4e156b3da8e7fc440a8
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-02-28 15:43:38 +0100

    Task12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,21 @@
 */
 void print_number(int n)
 {
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
+	if (n / 10 != 0)
+	{
 		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+	}
+	_putchar('0' + (n % 10) + '0');
 }
