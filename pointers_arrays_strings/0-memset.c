147f943d07c5e4d8b9f088b8140d9c3feb4cb303
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-03 13:17:29 +0100

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,27 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-/**
-* simple_print_buffer - print buffer in hexa
-* @buffer: the address of memory to print
-* @size: the size of the memory to print
-*
-* Return: Nothing
-*/
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i = 0;
-
-	while (i < size)
-	{
-		if (i % 10 == 0 && i)
-		{
-		printf("\n");
-		}
-		printf("0x%02x ", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
 
 /**
 * _memset - fils memory with a constant byte
