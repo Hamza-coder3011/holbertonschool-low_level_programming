2389c37e0ffb33207d33e4b82b61222b5d259644
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-03 13:52:30 +0100

    Task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include "main.h"
-#include <stdio.h>
 /**
 * _strchr - locates a character in a string
 * @s: the string to search
@@ -24,32 +23,4 @@ char *_strchr(char *s, char c)
 		return s;
 	}
 	return NULL;
-}
-/**
-* simple_print_buffer - prints buffer in hexa
-* @buffer: the address of memory to print
-* @size: the size of the memory to print
-*
-* Return: Nothing
-*/
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
 }
