f0d79703fbe94353603ba5e1321a2f7737e50ef8
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-03 13:34:43 +0100

    Task2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,4 +24,5 @@ char *leet(char *str);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
