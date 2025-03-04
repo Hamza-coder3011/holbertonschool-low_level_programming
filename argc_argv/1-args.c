2bd8b8836a34ba1477781a5dea55fc446463a788
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 16:47:39 +0100

    Task1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,6 +2,9 @@
 /**
 * main - Prints the number of arguments passed into the program
 *
+* @argc: The number of command line arguments
+* @argv: An array of strings representing the command line arguments
+*
 * Return: Always 0
 */
 int main(int argc, char *argv[])
