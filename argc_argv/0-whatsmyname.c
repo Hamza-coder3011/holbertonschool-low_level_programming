7e8465ac51a66cd3cbc817f11f89c3f86e3fb6ea
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 16:45:34 +0100

    Task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,6 +2,9 @@
 /**
 * main - Prints the name of the program followed by a new line
 *
+* @argc: The number of command line arguments
+* @argv: An array of strings representing the command line arguments
+*
 * Return: Always 0
 */
 int main(int __attribute__((unused)) argc, char *argv[])
