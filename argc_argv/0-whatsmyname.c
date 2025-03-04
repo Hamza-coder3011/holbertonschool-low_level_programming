af58ae2c69d110dd602a1a5084ac9e9551e07975
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 15:59:15 +0100

    Task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Prints the name of the program followed by a new line
+*
+* Return: Always 0
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
