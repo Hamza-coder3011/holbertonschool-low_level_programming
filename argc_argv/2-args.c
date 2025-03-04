9e5c5dddbd1491b85196faa2ffbc294484deb20a
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 16:14:38 +0100

    Task2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints all arguments passed to the program
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
