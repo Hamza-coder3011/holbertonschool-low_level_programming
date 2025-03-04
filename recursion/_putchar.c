98b3423970f46ba8457381463863b42d4d18f259
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 12:01:26 +0100

    Task0

---
+++ recursion/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+* _putchar - writes a character to stout
+* @c: The character to print
+* Return: On success 1, on error -1.
+*/
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
