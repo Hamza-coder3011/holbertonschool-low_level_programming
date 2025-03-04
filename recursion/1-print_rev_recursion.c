b2957501db1fbcaecb78fed940223dbdb435db91
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 15:12:29 +0100

    Task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * _print_rev_recursion - Prints a string in reverse using recursion.
 * @s: The string to be printed in reverse.
@@ -13,5 +12,5 @@ void _print_rev_recursion(char *s)
 
 	_print_rev_recursion(s + 1);
 
-	putchar(*s);
+	_putchar(*s);
 }
