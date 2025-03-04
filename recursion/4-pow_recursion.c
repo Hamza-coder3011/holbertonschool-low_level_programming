1ebbbf41e606c311d8e8c784097a831c41d0f3b0
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-04 14:48:12 +0100

    Task4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
 * @x: The base.
 * @y: The exponent
 *
-* Return: The value of x raised to the power of y. 
+* Return: The value of x raised to the power of y.
 *returns -1 if y is negative.
 */
 int _pow_recursion(int x, int y)
