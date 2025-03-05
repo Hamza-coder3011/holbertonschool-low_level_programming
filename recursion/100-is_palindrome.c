fffc938d8cd7c4840029084e567c0aca02d2007b
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-05 15:36:22 +0100

    Task7

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@
 * @s: The original string
 * @cleaned: The buffer to store the cleaned string
 *
-* Return: Length of cleaned string
+* Return: The length of cleaned string
 */
 int clean_string(char *s, char *cleaned)
 {
@@ -24,37 +24,34 @@ int clean_string(char *s, char *cleaned)
 
 	return (j);
 }
-/**
-* is_palindrome_recursive - Helper fonction to check if a string is palindrome
-* @s: The string to check
-* @left: The left index
-* @right: The right index
-*
-* Return: 1 if palindrome, 0 otherwise
-*/
-int is_palindrome_recursive(char *s, int left, int right)
-{
-	if (left >= right)
-		return (1);
-	if (s[left] != s[right])
-		return (0);
-	return (is_palindrome_recursive(s, left + 1, right - 1));
-}
 
 /**
-* is_palindrome - Checks if a string is a palindrome
-* @s: The string to check
+* is_palindrome - Checks if a string is a palindrome, ignoring spaces and case.
+* @s: The input string
 *
 * Return: 1 if palindrome, 0 otherwise
 */
 int is_palindrome(char *s)
 {
-	char cleaned[100];
+	char cleaned[1000];
 	int len = clean_string(s, cleaned);
+	int left, right;
 
 	if (len == 0)
 	{
 		return (1);
 	}
-	return (is_palindrome_recursive(cleaned, 0, len - 1));
+
+	left = 0;
+	right = len - 1;
+
+	while (left < right)
+	{
+		if (cleaned[left] != cleaned[right])
+			return (0);
+
+		left++;
+		right--;
+	}
+	return (1);
 }
