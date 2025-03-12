97d90135244f87a9a3a6a9a24bd9841d4131982c
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-12 11:27:00 +0100

    Task5

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 /**
 * argstostr - Concatenates all arguments of the program.
@@ -12,28 +11,39 @@
 */
 char *argstostr(int ac, char **av)
 {
-	int i, j, total_len = 0, index = 0;
+	int i, j, length = 0;
 	char *result;
 
 	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
-		total_len += strlen(av[i]) + 1;
-
-	result = malloc(sizeof(char) * (total_len + 1));
-	if (result == NULL)
-		return (NULL);
+	}
 
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			result[index++] = av[i][j];
+			length++;
 		}
-		result[index++] = '\n';
+		length++;
 	}
-	result[index] = '\0';
 
+	result = malloc(sizeof(char) * (length + 1));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[length++] = av[i][j];
+		}
+		result[length++] = '\n';
+	}
+
+	result[length] = '\0';
 	return (result);
 }
