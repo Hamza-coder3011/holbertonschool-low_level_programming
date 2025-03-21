25da8de32d30ba85665affc24939aa9deef4e391
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-21 09:35:35 +0100

    Task4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 /**
-* _strpbrk - searches a string for anny of a set of bytes
+* _strpbrk - searches a string for any of a set of bytes
 * @s: input string to search
 * @accept: character to match
 *
@@ -10,14 +10,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-			{
+			if (s[i] == accept[j]);
 			return (&s[i]);
-			}
 		}
 	}
 	return (NULL);
