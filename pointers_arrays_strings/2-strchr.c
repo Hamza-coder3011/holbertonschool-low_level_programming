2c3c090f6619fc712d83367a124afd8dd3d4511c
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-21 09:28:25 +0100

    Task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,23 +5,22 @@
 * @s: The string to search
 * @c: The character to locate
 *
-* Return: A pointer to the first occurrence of the charactere c
+* Return: A pointer to the first occurrence of the charactere c,
+*	or NULL if the charactere is not found.
 */
 char *_strchr(char *s, char c)
 {
+	if (s == NULL)
+		return (NULL);
 	while (*s)
 	{
 		if (*s ==c)
-		{
 			return (s);
-		}
 		s++;
 	}
 
 	if (*s == c)
-	{
 		return (s);
-	}
 
 	return (NULL);
 }
