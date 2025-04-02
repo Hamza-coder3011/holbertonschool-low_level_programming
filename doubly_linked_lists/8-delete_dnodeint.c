aef2bbba6728a29333d32d0a3dd1beb593ecb1ae
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-02 13:09:17 +0200

    update if (index == 0) return 1 not -1

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (current->next)
 			current->next->prev = NULL;
 		free(current);
-		return (-1);
+		return (1);
 	}
 
 	while (current && i < index)
