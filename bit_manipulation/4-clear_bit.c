4947f2d858060b03631e1c7dbd273e7853c547df
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-04 12:58:28 +0200

    update

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -4,18 +4,15 @@
 * clear_bit - Sets the value of a bit to 0 at a given index
 * @n: The number in which the bit will be cleared
 * @index: The index of the bit to be cleared, starting from 0
+*
+* Return: 1 if it worked, or -1 if an error occurred
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
-	{
-		return (-1);
-	}
+	unsigned int max_index = sizeof(unsigned long int) * 8;
 
-	if (index >= (sizeof(unsigned long int) * 8))
-	{
+	if (!n || index >= max_index)
 		return (-1);
-	}
 
 	*n &= ~(1UL << index);
 
