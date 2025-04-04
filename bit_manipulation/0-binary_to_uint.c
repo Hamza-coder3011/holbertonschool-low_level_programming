2534a9ea63bbb274c41506548769240662134ced
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-04 13:18:57 +0200

    Wrong update

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == NULL)
+	if (b == NULL || b[0] == '\0')
 		return (0);
 
-	for (; *b != '\0'; b++)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
