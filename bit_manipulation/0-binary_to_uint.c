6e953f56529a44958f941aae781d45a6841b59e8
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-04 13:13:21 +0200

    try to fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == NULL || b[0] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while (*b)
+	for (; *b != '\0'; b++)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
